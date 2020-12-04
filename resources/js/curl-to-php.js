efbac88cd4c2ab8b25e57117f81f551e878e4bf6
mkp95 <mkp95@users.noreply.github.com>
2020-12-04 22:52:10 +0530

    Update curl-to-php.js
    Test

--- resources/js/curl-to-php.js
+++ resources/js/curl-to-php.js
@@ -156,7 +156,7 @@ if( req.url.substring(0, 5) == "https" ) {
 			// render PHP code to put all the data in the body, concatenating if necessary
 			if (ioReaders.length == 1 && typeof varName == 'undefined') {
 				//If variable have code ".. -d attributes='{...", delete quotes.
-				ioReaders[0] = ioReaders[0].replace(/\=[\'\"]\{([^$]+)\}[\'\"]/, '={$1}');
+				ioReaders[0] = ioReaders[0].replace(/\=[\']\{([^$]+)\}[\']/, '={$1}');
 				php += 'curl_setopt($ch, CURLOPT_POSTFIELDS, '+ioReaders[0]+');\n';
 			} else if (ioReaders.length > 0) {
 				php += '$post = array(\n    ';
